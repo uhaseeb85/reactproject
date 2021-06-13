aae342c72331bcc64503ce3e8c477dad19c137d0
uhaseeb85 <uhaseeb85@gmail.com>
2021-06-12 20:43:52 -0700

    changes

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,13 +2,47 @@ import React, { useState } from 'react';
 import './Login.css';
 import PropTypes from 'prop-types';
 
+/**
+ * 
+ * loginUser
+ * 
+ * @param {*} credentials 
+ * @returns token
+ */
+async function loginUser(credentials) {
+  return fetch('http://localhost:8080/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  })
+    .then(data => data.json())
+ }
+
+ /**
+  * Login
+  * 
+  * @param {*} param0 
+  * @returns 
+  */
 export default function Login({ setToken }) {
     const [username, setUserName] = useState();
     const [password, setPassword] = useState();
+
+    const handleSubmit = async e => {
+      e.preventDefault();
+      const token = await loginUser({
+        username,
+        password
+      });
+      setToken(token);
+    }
+
   return(
     <div className="login-wrapper">
     <h1>Please Log In</h1>
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>
         <p>Username</p>
         <input type="text" onChange={e => setUserName(e.target.value)}/>
