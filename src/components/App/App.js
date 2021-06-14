20e0d2f510a6a459304df3757d1ef517d56729b2
uhaseeb85 <uhaseeb85@gmail.com>
2021-06-14 15:01:57 -0700

    login

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,32 +1,34 @@
-import React, { useState } from 'react';
-import './App.css';
-import Login from '../Login/Login';
+import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css';
 import Dashboard from '../Dashboard/Dashboard';
+import Login from '../Login/Login';
 import Preferences from '../Preferences/Preferences';
+import useToken from './useToken';
 
 function App() {
-    const [token, setToken] = useState();
 
-    if(!token) {
-        return <Login setToken={setToken} />
-      }
+  const { token, setToken } = useToken();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
 
   return (
     <div className="wrapper">
-    <h1>Application</h1>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/dashboard">
-          <Dashboard />
-        </Route>
-        <Route path="/preferences">
-          <Preferences />
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  </div>
-);
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/preferences">
+            <Preferences />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
